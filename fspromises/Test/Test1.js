cc2c8b1cc40ad276efb83fd45f970091836ff1c0
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-09-24 17:23:09 +0530

    feat: add Test File for fsPromises

---
+++ fspromises/Test/Test1.js
@@ -0,0 +1,7 @@
+/*Using promises and the fs module's asynchronous functions, do the following:
+        1. Create a directory of random JSON files
+        2. Delete those files simultaneously */
+
+const asyncOperation = require('../problem1');
+
+asyncOperation(__dirname);
